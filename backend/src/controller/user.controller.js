9d4099ffab6c9f5a816895289e7eb4ab371852ce
Tushar <tushar990022@gmail.com>
2025-03-16 23:46:33 +0530

    refactor: simplify cookie setting in user registration by removing options

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -159,8 +159,8 @@ const registerUser = asyncHandler(async (req, res) => {
 
     return res
       .status(200)
-      .cookie("accessToken", accessToken, options )
-      .cookie("refreshToken", refreshToken, options)
+      .cookie("accessToken", accessToken )
+      .cookie("refreshToken", refreshToken)
       .json(
         new ApiResponse(
           200,

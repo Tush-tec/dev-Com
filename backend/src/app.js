5e3076daa0a4884fbfe4426d92b53d291d49899b
Tushar <tushar990022@gmail.com>
2025-03-16 14:28:17 +0530

    refactor: clean up CORS middleware configuration by removing unnecessary method and header specifications

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,8 +23,7 @@ app.use(
        
             origin: process.env.CORS_ORIGIN_FROM_PRODUCTION,
             credentials:true,
-            methods: ["GET", "POST", "PUT", "DELETE"],
-            allowedHeaders: ["Content-Type", "Authorization"],   
+            
         }
     )
 )

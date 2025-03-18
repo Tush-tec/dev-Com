69fd79c956d1788a1f3435b91f473412c41d6a20
Tushar <tushar990022@gmail.com>
2025-03-18 11:11:21 +0530

    refactor: streamline database connection handling in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,20 @@
-// import { app } from "./app.js";
-// import connectionInstance from "./config/db.js";
-
-// connectionInstance()
-// .then(() => {
-//     app.listen(process.env.PORT, ()=>{
-//         console.log(`Server is running on port ${process.env.PORT}`);
-//     })
-// })
-// .catch((error)=> {
-
-//     console.log('Error connecting to database', error || error.message);
-    
-// }) 
-
 import { app } from "./app.js";
 import connectionInstance from "./config/db.js";
 
+connectionInstance()
+.then(() => {
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error)=> {
 
-await connectionInstance().catch((error) => {
-    console.error('Error connecting to database', error || error.message);
-});
+    console.log('Error connecting to database', error || error.message);
+    
+}) 
 
 
 
 
 
-export default app;
+

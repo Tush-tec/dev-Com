cda2d5aa0d7098bf1d1029a66b2c1dc8b5d53a95
Tushar <tushar990022@gmail.com>
2025-03-18 11:22:19 +0530

    refactor: remove unused validation files and streamline helper functions

--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -1,6 +1,5 @@
 import fs from "fs";
 import mongoose from "mongoose";
-import logger from "./logger/winston.logger.js";
 
 
 
@@ -73,14 +72,7 @@ export const getLocalPath = (fileName) => {
  * @param {string} localPath
  * @description Removed the local file from the local file system based on the file path
  */
-export const removeLocalFile = (localPath) => {
-  fs.unlink(localPath, (err) => {
-    if (err) logger.error("Error while removing local files: ", err);
-    else {
-      logger.info("Removed local: ", localPath);
-    }
-  });
-};
+
 
 /**
  * @param {import("express").Request} req
@@ -91,34 +83,7 @@ export const removeLocalFile = (localPath) => {
  * * In product creation process the images are getting uploaded before product gets created.
  * * Once images are uploaded and if there is an error creating a product, the uploaded images are unused.
  * * In such case, this function will remove those unused images.
- */
-export const removeUnusedMulterImageFilesOnError = (req) => {
-  try {
-    const multerFile = req.file;
-    const multerFiles = req.files;
 
-    if (multerFile) {
-      // If there is file uploaded and there is validation error
-      // We want to remove that file
-      removeLocalFile(multerFile.path);
-    }
-
-    if (multerFiles) {
-      /** @type {Express.Multer.File[][]}  */
-      const filesValueArray = Object.values(multerFiles);
-      // If there are multiple files uploaded for more than one fields
-      // We want to remove those files as well
-      filesValueArray.map((fileFields) => {
-        fileFields.map((fileObject) => {
-          removeLocalFile(fileObject.path);
-        });
-      });
-    }
-  } catch (error) {
-    // fail silently
-    logger.error("Error while removing image files: ", error);
-  }
-};
 
 /**
  *
